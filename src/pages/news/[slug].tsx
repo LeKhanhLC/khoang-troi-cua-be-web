edd9bfc80b9ec6a9ed33bc10d617812dffb8667c
leKhanhGithub <khanhcong1909@gmail.com>
2023-10-19 07:56:47 +0700

    fix slide

--- src/pages/news/[slug].tsx
+++ src/pages/news/[slug].tsx
@@ -102,7 +102,7 @@ const News: NextPage<Props> = ({ news, rightOtherNews, bottomOtherNews }) => {
           </section>
         </Container>
       </Stack>
-      <Stack width="100%" alignItems="center">
+      <Stack alignItems="center" px={7}>
         <SlideNews slideNewsData={slideNewsData} />
       </Stack>
     </>
